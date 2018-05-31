eb0d340baf4acce3765622471b5481d2fb6775e9
Boyd Johnson <bjohnson@bitwise.io>
2018-05-31 13:33:08 -0500

    Fix incorrect check of None python object
    Signed-off-by: Boyd Johnson <bjohnson@bitwise.io>

--- validator/src/scheduler/py_scheduler.rs
+++ validator/src/scheduler/py_scheduler.rs
@@ -124,7 +124,8 @@ impl Scheduler for PyScheduler {
                     let batch_result: cpython::PyObject = self.py_scheduler
                         .call_method(py, "get_batch_execution_result", (id,), None)
                         .expect("No method get_batch_execution_result on python scheduler");
-                    if batch_result.is_true(py).unwrap() {
+
+                    if batch_result != cpython::Python::None(py) {
                         let result = batch_result.extract::<BatchResult>(py).unwrap();
 
                         let txn_results: Vec<TransactionResult> = self.py_scheduler
