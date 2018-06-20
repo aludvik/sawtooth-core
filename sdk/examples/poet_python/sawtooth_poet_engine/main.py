a6f643f41adcda4c193d35f10763e024ae5dad6e
Adam Ludvik <ludvik@bitwise.io>
2018-06-20 09:51:19 -0500

    Add missing log config import
    Signed-off-by: Adam Ludvik <ludvik@bitwise.io>

--- sdk/examples/poet_python/sawtooth_poet_engine/main.py
+++ sdk/examples/poet_python/sawtooth_poet_engine/main.py
@@ -21,6 +21,7 @@ import logging
 from sawtooth_sdk.processor.log import init_console_logging
 from sawtooth_sdk.processor.log import log_configuration
 from sawtooth_sdk.processor.config import get_log_dir
+from sawtooth_sdk.processor.config import get_log_config
 
 from sawtooth_sdk.consensus.zmq_driver import ZmqDriver
 from sawtooth_poet_engine.engine import PoetEngine
